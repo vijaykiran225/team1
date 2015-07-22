0c31062e79fbbb500448ba283d8e0c55af215585
unknown <test@L-MAA-25004272.paypalcorp.com>
2015-07-22 18:02:30 +0530

    differnce

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 using namespace std;
-
+int* getIntegerArray(int number, int numDigits);
+void printArray(int *array, int size);
 class Odometer
 {
     public:
@@ -9,7 +10,7 @@ class Odometer
         int getCurrentValue();
         int nextValue(int value);
         int prevValue(int value);
-        int distDiff(Odometer, Odometer);
+        int distDiff(int, int);
         bool checkAscending(int num);
 
     private:
@@ -30,15 +31,15 @@ Odometer::Odometer(int numDigits)
 }
 int Odometer::prevValue(int val)
 {
-    int numArray[] = {0, 3, 5, 7, 8, 9};
+    int *numArray = getIntegerArray(val, nDigits);
     int pos = 0;
-    bool flag = true;
-    while (pos < nDigits && flag) {
-        int curr = nDigits - pos - 1;
+    bool gotPrev = true;
+    while (pos < nDigits + 1 && gotPrev) {
+        int curr = nDigits  - pos;
         int prev = curr - 1;
         if ( numArray[curr] - 1 > numArray[prev] ) {
             numArray[curr] -= 1;
-            flag = false;
+            gotPrev = false;
         }
         else {
             numArray[curr] = 9 - pos;
@@ -46,16 +47,66 @@ int Odometer::prevValue(int val)
         pos++;
     }
     int nextNum = 0;
-    for (int i = 1; i < nDigits; i++ ) {
+    for (int i = 1; i < nDigits + 1; i++ ) {
         nextNum = nextNum * 10 + numArray[i];
     }
     return nextNum;
 }
 
+int Odometer::nextValue(int val)
+{
+    int *numArray = getIntegerArray(val, nDigits);
+    int pos = 0;
+    bool gotNext = true;
+    while (pos < nDigits + 1 && gotNext) {
+        int curr = nDigits  - pos;
+        //int prev = curr - 1;
+        if ( numArray[curr] - 1 > numArray[prev] ) {
+            numArray[curr] -= 1;
+            gotPrev = false;
+        }
+        else {
+            numArray[curr] = 9 - pos;
+        }
+        pos++;
+    }
+    int prevNum = 0;
+    for (int i = 1; i < nDigits + 1; i++ ) {
+        prevNum = prevNum * 10 + numArray[i];
+    }
+    return prevNum;
+}
 
+
+int Odometer::distDiff(int o1, int o2)
+{
+    int count = 0;
+    while ( o1 > o2 ) {
+        o1 = prevValue(o1);
+        count++;
+    }
+    return count;
+}
+int* getIntegerArray(int number, int numDigits) {
+	int *intArr = new int(numDigits + 1);
+	int i;
+
+	for (i = numDigits; i > 0; --i) {
+		intArr[i] = number % 10;
+		number /= 10;
+	}
+	intArr[0] = 0;
+	return intArr;
+}
+void printArray(int *array, int size) {
+	for (int i = 0; i <= size; ++i) {
+		cout << i<<"is "<<array[i] << endl;
+	}
+}
 int main()
 {
-    Odometer odMeter(6);
-    cout << "Previous : " << odMeter.prevValue(12345) << endl;
+    Odometer odMeter(5);
+    cout << "Previous : " << odMeter.prevValue(34569) << endl;
+    cout << "Difference : " << odMeter.distDiff(13458, 12345) << endl;
     return 0;
 }
